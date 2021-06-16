05610b92e5c7828b29676924bcc0cc3c3ff85efa
BuvanPranesh <84124379+BuvanPranesh@users.noreply.github.com>
2021-06-16 11:49:47 +0530

    Delete flask_sqlalchemy directory

--- flask_sqlalchemy/My_SQLAlchemy/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import *
-from flask_sqlalchemy import SQLAlchemy
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///studentsdetails.db'
-app.config['SECRET_KEY'] = "StudentDetails"
-db = SQLAlchemy(app)
-
-
-from My_SQLAlchemy import views
--- flask_sqlalchemy/run.py
+++
@@ -1,7 +0,0 @@
-from My_SQLAlchemy import app, db
-
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
-
