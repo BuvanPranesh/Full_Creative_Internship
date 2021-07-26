664fe1068a3465e2332bbc709610489892a659c1
BuvanPranesh <buvanpranesh.kt@gmail.com>
2021-07-26 20:11:19 +0530

    Register For Vaccine

--- Project_Register_Vaccine/project.py
+++ Project_Register_Vaccine/project.py
@@ -1,6 +1,7 @@
 from flask import *
 from google.cloud import ndb, datastore
 import os
+import datetime
 from google.cloud.datastore import *
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'vaccine-booking.json'
@@ -14,11 +15,19 @@ class Place(ndb.Model):
 
 
 class People(ndb.Model):
+    city = ndb.StringProperty()
+    vaccine_centre = ndb.StringProperty()
     name = ndb.StringProperty()
     age = ndb.StringProperty()
     aadhaar_no = ndb.StringProperty()
     phone_no = ndb.StringProperty()
     address = ndb.StringProperty()
+    time = ndb.StringProperty()
+
+
+client = ndb.Client()
+client.context()
+global id1
 
 
 @app.route('/')
@@ -26,20 +35,23 @@ def login():
     return render_template('project.html')
 
 
+@app.route('/booking')
+def book():
+    global id1
+    id1 = request.args.get('id')
+    return render_template('register.html')
+
+
 @app.route('/centre', methods=['POST'])
 def center():
-    client = ndb.Client()
-    with client.context():
-        res = Place(city=request.form.get('city'), vaccine_centre=request.form.get('center'),
-                    slots_available=request.form.get('slots'))
-        res.put()
+    res = Place(city=request.form.get('city'), vaccine_centre=request.form.get('center'),
+                slots_available=request.form.get('slots'))
+    res.put()
 
 
 @app.route('/search')
 def search():
     res_li = []
-    li2 = []
-    client = ndb.Client()
     with client.context():
         res = Place.query().filter(Place.city == request.args.get('field')).fetch()
         j = 1
@@ -52,29 +64,35 @@ def search():
 
 @app.route('/register', methods=['POST'])
 def register():
-    print(request.form.get("name"))
-
-
-def register1(aa1):
-    res1 = Place.query()
-    for i in res1:
-        if i.key.id() == aa1:
-            i.slots_available = str(int(i.slots_available) - 1)
-            i.put()
-    return "<html><h1>You have Registered Successfully For vaccination<h1><h2><a href='/'>Home Page</a></h2></html>"
+    x = datetime.datetime.now()
+    if request.form.get('name'):
+        with client.context():
+            res1 = Place.query()
+            for i in res1:
+                if i.key.id() == int(id1):
+                    if int(i.slots_available) >= 1:
+                        i.slots_available = str(int(i.slots_available)-1)
+                        i.put()
+                        res = People(name=request.form.get('name'), age=request.form.get('age'),
+                                     aadhaar_no=request.form.get('aadhaar_no'), phone_no=request.form.get('phone_no'),
+                                     address=request.form.get('address'), city=i.city, vaccine_centre=i.vaccine_centre,
+                                     time=str(x))
+                        res.put()
+                    else:
+                        return "<html><body><h1>Vaccine Not available at this centre</h1>" \
+                               "<h2><a href='/'>Click to home page</a></h2></body></html>"
+        return redirect(url_for('login'))
 
 
 @app.route('/displayRegistration')
 def display():
     res_li = []
-    c = 1
-    client = ndb.Client()
     with client.context():
         res = People.query()
         for j in res:
-            res_li.append({"total_registration": c, "name": j.name, "age": j.age, "aadhaar_no": j.aadhaar_no,
-                           "phone_no": j.phone_no, "address": j.address})
-            c = c + 1
+            res_li.append({"city": j.city, "vaccine_centre": j.vaccine_centre,
+                           "name": j.name, "age": j.age, "aadhaar_no": j.aadhaar_no, "phone_no": j.phone_no,
+                           "address": j.address, "time": j.time})
         return jsonify({"register_vaccine": res_li})
 
 
--- Project_Register_Vaccine/static/javascript/project.js
+++ Project_Register_Vaccine/static/javascript/project.js
@@ -1,133 +1,155 @@
-var row_count=1;
-     function search_form(event){
-        event.preventDefault();
-        document.getElementById('btn-container').classList.add('details1');
-        document.getElementById('search-city').classList.remove('details1');
-     }
-
-     function create_table(v1,v2,v3){
-     document.getElementById('display_results').classList.remove('details1');
-     var row = document.getElementById('results');
-     console.log(row_count)
-     var row_create = row.insertRow(row_count);
-     var val_city = row_create.insertCell(0);
-     var val_vaccine_center = row_create.insertCell(1);
-     var val_slots_available = row_create.insertCell(2);
-     var Book_Now = row_create.insertCell(3);
-     val_city.innerHTML = v1;
-     val_vaccine_center.innerHTML = v2;
-     val_slots_available.innerHTML = v3;
-     Book_Now.innerHTML ="<button id='book_now' onclick='register()'>Book Now</button>";
-     row_count+=1;
-     res.appendChild(row_create);
-     }
+ var table_content="";
+        var row_count=1;
+        var table_header="";
+        var table_footer="";
+        var display_count=1;
 
 
-     function register(){
-        document.getElementById('register').classList.remove("details1");
-        document.getElementById('register_vaccine').addEventListener('click',register_vaccine);
-        }
+        document.getElementById('city-search').addEventListener('click',search);
 
 
-     function register_vaccine(){
-        var name = document.getElementById('name1').value;
-        var age = document.getElementById('age1').value;
-        var aadhaar_no = document.getElementById('aadhaar_no1').value;
-        var phone_no = document.getElementById('phone_no1').value;
-        var address = document.getElementById('address1').value;
-        var final = JSON.stringify({name:name,age:age,aadhaar_no:aadhaar_no,phone_no:phone_no,address:address})
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST','/register',true);
-        xhr.send(final);
-     };
-
-
-     function searchCity(event){
-        event.preventDefault();
-        var xhr = new XMLHttpRequest();
-        var x = document.getElementById('field1').value;
-        var url = '/search?field='+x;
-        console.log(x);
-        xhr.open('GET',url,true);
-        xhr.onload = function() {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-               var response_server = JSON.parse(xhr.responseText);
-               for(var i=0; i<response_server.vaccine.length; i++){
-                    var city = response_server.vaccine[i].city;
-                    var vaccine_place = response_server.vaccine[i].vaccine_center;
-                    var slots_remaining= response_server.vaccine[i].slots_available;
-                    var res = document.getElementById('res');
-                    res.innerHTML = "";
-                    create_table(city,vaccine_place,slots_remaining);
-               }
+        function search(event){
+            row_count=1;
+            table_header="";
+            table_content="";
+            table_footer="";
+            document.getElementById('display_results').classList.add('details1');
+            document.getElementById('search_results').classList.remove('details1');
+            document.getElementById('search_results').innerHTML="";
+            event.preventDefault();
+            searchCity();
             }
-       }
-        xhr.send();
-    }
-
-    function check(){
-    if (row_count>=2){
-    document.getElementById('display_results').classList.remove('details1');
-    }
-    }
-    document.getElementById('city-search').addEventListener('click',searchCity);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    function check(){
-        row_count=1;
-        document.getElementById('display_results').innerHTML ="";
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        function register_vaccine(){
-            var name = document.getElementById('name1').value;
-            var age = document.getElementById('age1').value;
-            var aadhaar_no = document.getElementById('aadhaar_no1').value;
-            var phone_no = document.getElementById('phone_no1').value;
-            var address = document.getElementById('address1').value;
-            var final = JSON.stringify({name:name,age:age,aadhaar_no:aadhaar_no,phone_no:phone_no,address:address})
+        function searchCity(){
+            document.getElementById('search_results').innerHTML="";
             var xhr = new XMLHttpRequest();
-            xhr.open('POST','/register',true);
-            xhr.send(final);
+            var x = document.getElementById('field1').value;
+            var url = '/search?field='+x;
+            xhr.open('GET',url,true);
+            xhr.onload = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    var response_server = JSON.parse(xhr.responseText);
+                    for(var i=0; i<response_server.vaccine.length; i++){
+                        var no = response_server.vaccine[i].no;
+                        var city = response_server.vaccine[i].city;
+                        var vaccine_place = response_server.vaccine[i].vaccine_centre;
+                        var slots_remaining = response_server.vaccine[i].slots_available;
+                        var key_id_vaccine = response_server.vaccine[i].key_id;
+                        create_table(no,city,vaccine_place,slots_remaining,key_id_vaccine);
+                    }
+                }
+            }
+            xhr.send();
         }
+
+
+        function create_table(v1,v2,v3,v4,v5){
+         document.getElementById('search_results').classList.remove('details1');
+         console.log(v5);
+            if(row_count==1){
+                var table_header = '<table style="width:75%">\n';
+                table_content+='<tr>';
+                table_content+='<th align="center">'+ "No"+'</th>';
+                table_content+='<th align="center">'+ "City" +'</th>';
+                table_content+='<th align="center">'+ "Vaccine Center" +'</th>';
+                table_content+='<th align="center">'+ "Vaccine Available" +'</th>';
+                table_content+='<th align="center">'+ "Book Vaccine" +'</th>';
+                table_content+= '</tr>\n';
+                var table_footer= '</table>';
+                document.getElementById('search_results').innerHTML = table_header+ table_content+ table_footer;
+                row_count+=1;
+            }
+                console.log(v5);
+                var table_header = '<table style="width:75%">\n';
+                table_content+= '<tr>';
+                table_content+='<td align="center" >'+ v1 +'</td>';
+                table_content+='<td align="center">'+ v2 +'</td>';
+                table_content+='<td align="center" id=row_count>'+ v3 +'</td>';
+                table_content+='<td align="center">'+ v4 +'</td>';
+                table_content+='<td align="center">'+ `<button onclick=register('${v5}')>Book Now</button>` +'</td>';
+                table_content+='</tr>\n';
+                var table_footer='</table>';
+                document.getElementById('search_results').innerHTML = table_header + table_content + table_footer;
+                row_count+=1;
+        }
+
+
+        function register(v5){
+            document.getElementById('search_results').classList.add('details1');
+            document.getElementById('display_results').classList.add('details1');
+            window.open('/booking?id='+v5);
+        }
+            document.getElementById('registered_user').addEventListener('click',registration);
+
+
+        function registration(event){
+            event.preventDefault();
+            display_count=1;
+            table_header="";
+            table_content="";
+            table_footer="";
+            document.getElementById('search_results').classList.add('details1');
+            document.getElementById('display_results').classList.remove('details1');
+            document.getElementById('display_results').innerHTML="";
+            displayRegistration();
+        }
+
+
+        function displayRegistration(){
+            var xhr = new XMLHttpRequest();
+            var url = '/displayRegistration';
+            xhr.open('GET',url,true);
+            xhr.onload = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    var response_server = JSON.parse(xhr.responseText);
+                    for(var i=0; i<response_server.register_vaccine.length; i++){
+                        var city = response_server.register_vaccine[i].city;
+                        var vaccine_centre = response_server.register_vaccine[i].vaccine_centre;
+                        var name = response_server.register_vaccine[i].name;
+                        var age = response_server.register_vaccine[i].age;
+                        var aadhaar_no = response_server.register_vaccine[i].aadhaar_no;
+                        var phone_no = response_server.register_vaccine[i].phone_no;
+                        var address = response_server.register_vaccine[i].address;
+                        var time = response_server.register_vaccine[i].time;
+                        display_results(city,vaccine_centre,name,age,aadhaar_no,phone_no,address,time);
+                    }
+                }
+            }
+            xhr.send();
+        }
+
+
+    function display_results(v1,v2,v3,v4,v5,v6,v7,v8){
+     document.getElementById('display_results').classList.remove('details1');
+            if(display_count==1){
+                var table_header='<table style="width:75%">\n';
+                table_content+='<tr>';
+                table_content+='<th align="center">'+ "City" +'</th>';
+                table_content+='<th align="center">'+ "Vaccine Centre" +'</th>';
+                table_content+='<th align="center">'+ "Name" +'</th>';
+                table_content+='<th align="center">'+ "Age" +'</th>';
+                table_content+='<th align="center">'+ "Aadhaar No" +'</th>';
+                table_content+='<th align="center">'+ "Phone No" +'</th>';
+                table_content+='<th align="center">'+ "Address" +'</th>';
+                table_content+='<th align="center">'+ "Time" +'</th>';
+                table_content+='</tr>\n';
+                var table_footer='</table>';
+                document.getElementById('display_results').innerHTML = table_header + table_content + table_footer;
+                display_count+=1;
+            }
+                var table_header = '<table style="width:75%">\n';
+                table_content+='<tr>';
+                table_content+='<td align="center">'+ v1 +'</td>';
+                table_content+='<td align="center">'+ v2 +'</td>';
+                table_content+='<td align="center">'+ v3 +'</td>';
+                table_content+='<td align="center">'+ v4 +'</td>';
+                table_content+='<td align="center">'+ v5 +'</td>';
+                table_content+='<td align="center">'+ v6 +'</td>';
+                table_content+='<td align="center">'+ v7 +'</td>';
+                table_content+='<td align="center">'+ v8 +'</td>';
+                table_content+='</tr align="center">\n';
+                var table_footer='</table>';
+                document.getElementById('display_results').innerHTML = table_header + table_content + table_footer;
+                display_count+=1;
+         }
